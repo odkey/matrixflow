b0c81edb00931ea7a5e58f4522e5e38a50ae12bb
tdual <tdualdir@gmail.com>
2018-05-31 22:38:08 +0900

    make log.py not put files.

--- src/log.py
+++ src/log.py
@@ -7,7 +7,7 @@ def log_debug(msg):
     logging.basicConfig(
         format='%(asctime)s %(message)s',
         datefmt='%Y/%m/%d %p %I:%M:%S',
-        filename="/root/share/var/log/error.log",
+        #filename="/root/share/var/log/error.log",
         level=logging.DEBUG
     )
     logging.debug(msg)
